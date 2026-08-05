cde93ccaed33215978191313aa766cd006056cff
tita2601 <titaclass4@gmail.com>
2026-08-05 20:40:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
-print("OPENTIX monitor start")
+import requests
+import os
 
-print("GitHub Actions OK")
+
+def send_line(message):
+
+    token = os.environ["LINE_TOKEN"]
+    user_id = os.environ["LINE_USER"]
+
+    url = "https://api.line.me/v2/bot/message/push"
+
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+
+    data = {
+
+        "to": user_id,
+
+        "messages": [
+            {
+                "type": "text",
+                "text": message
+            }
+        ]
+
+    }
+
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=data
+    )
+
+
+    print(response.status_code)
+    print(response.text)
+
+
+
+if __name__ == "__main__":
+
+    send_line(
+        "🎉 OPENTIX監控系統測試成功！"
+    )
