3242715a911a39064c88bdb9d9d1efe3f68e6372
tita2601 <titaclass4@gmail.com>
2026-08-06 23:36:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,23 +33,32 @@ with sync_playwright() as p:
     # 再等一下讓畫面完全載入
     page.wait_for_timeout(3000)
 
-    print("開始點擊『查看』")
+print("開始點擊『查看』")
 
-    page.get_by_text("查看").first.click(timeout=10000)
+buttons = page.get_by_text("查看")
 
-    page.wait_for_timeout(5000)
+print("查看總數：", buttons.count())
 
-    print("===== 點擊查看後 =====")
+for i in range(buttons.count()):
+    print(
+        i,
+        buttons.nth(i).is_visible()
+    )
+
+
+buttons.nth(1).click(timeout=10000)
+
+print("===== 點擊查看後 =====")
 
     text = page.locator("body").inner_text()
 
-    print(text[-3000:])
+print(text[-3000:])
 
-    print("網頁文字長度：")
-    print(len(text))
+print("網頁文字長度：")
+print(len(text))
 
-    print("網頁最後1000字：")
-    print(text[-1000:])
+print("網頁最後1000字：")
+print(text[-1000:])
 
 
     browser.close()
